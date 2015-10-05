23d9a4d8f1e11205e1f8b275df16e1e84a6e74c1
luis <luis.gil@tajamar365.com>
2015-10-05 12:54:39 +0200

    Final

---
+++ WebApplication1/dragdrop.js
@@ -0,0 +1,27 @@
+﻿if (!sessionStorage.getItem("nombre"))
+    location.replace("index.html");
+
+
+var idActual = undefined;
+function drag(evt) {
+    idActual= evt.target.id;
+}
+function allowDrop(evt) {
+    evt.preventDefault();
+}
+function drop(evt) {
+    evt.preventDefault();
+    //var data = evt.dataTransfer.getData("objeto");
+    document.getElementById("destino")
+        .appendChild(document.getElementById(idActual));
+    idActual = undefined;
+}
+
+var imgs = document.querySelectorAll("img");
+var destino = document.querySelector("#destino");
+
+for (var i = 0; i < imgs.length; i++) {
+    imgs[i].addEventListener("dragstart",drag);
+}
+destino.addEventListener("dragover", allowDrop);
+destino.addEventListener("drop", drop);
---
+++ WebApplication1/inicio.js
@@ -0,0 +1,17 @@
+﻿if (sessionStorage.getItem("nombre")) {
+    location.replace("imagenes.html");
+}
+
+
+function login() {
+    if (document.getElementById("txtNom").value == "") {
+        alert("Nombre vacio");
+        return;
+    }
+    sessionStorage.setItem("nombre",
+        document.getElementById("txtNom").value);
+    location.replace("imagenes.html");
+
+}
+
+document.getElementById("btnOk").addEventListener("click",login);
